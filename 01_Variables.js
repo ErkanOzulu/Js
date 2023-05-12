6b2537f10409496d2221d7deb578fede518e80fe
Erkan <ErkanOzulu@users.noreply.github.com>
2023-05-12 17:03:08 +0300

    variables

---
+++ 01_Variables.js
@@ -0,0 +1,34 @@
+console.log("Hello JavaScript Learners");
+
+//declaring variables
+/**
+ * Js is dynamic typing language
+ */
+
+let a;
+console.log(typeof a);
+a=5;  //infers that a is a number
+console.log(typeof a);
+a='BootCamp';  //infers that a is String
+console.log(typeof a);
+a=true; //infers that a is boolean
+console.log(typeof a);
+
+
+let x=5, y='Oscar';
+console.log(x+y) //concatination : 5Oscar
+console.log(x*y); //Nan
+
+// Naming Rules : same with Java naming rules
+
+//constant variables
+
+const number=35;
+
+//TypeError: Assignment to constant variable
+//number=45;
+
+console.log(number);
+
+
+
---
+++ Test.js
@@ -0,0 +1,3 @@
+console.log("Hello World");
+console.log("problem is solved");
+console.log("This is a new line");
