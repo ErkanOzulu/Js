70d20542c1a2c28d42ddcecbd2c0e2256d508523
Erkan <ErkanOzulu@users.noreply.github.com>
2023-05-14 12:57:52 +0300

    arrays loops

--- 05_Arrays.js
+++ 05_Arrays.js
@@ -6,14 +6,50 @@
  * 2. {} ----> creates an object
  */
 
-let scores=new Array();
-let numbers= new Array(10);
-let emptyArray=[];
-let myNumbers=new Array(1,2,3,4,5);
-let colors=['red', 'blue','green'];
+let scores = new Array(); // creates an empty Array
+let numbers = new Array(10); // creates an array with size 10
+let emptyArray = []; // creates an empty Array
+let myNumbers = new Array(1,2,3,5,8,13);
+let colors = ['red','green','blue'];
+
 
 console.log(scores);
 console.log(numbers);
 console.log(emptyArray);
 console.log(myNumbers);
 console.log(colors);
+
+// print second element in an array
+console.log(colors[1]); // indexed based
+
+// add element to an Array at the end
+colors.push("white");
+console.log(colors);
+
+// add element to beginnig of the Array
+colors.unshift("orange"); 
+console.log(colors);
+
+console.log(colors.pop()); // giving the last element and removing at the same time: white
+console.log(colors); 
+
+// the size of the array
+console.log(colors.length);
+for (let i = 0; i < colors.length ; i++){
+   console.log(colors[i]);
+}
+
+// loop with values of Array
+for (let value of colors) {
+    console.log(value);
+}
+
+colors.forEach(element => {
+    console.log(element);
+});
+
+// can we add multiple type of Data? YES
+let soupOfData = [5, "Oscar", true, undefined]; // you can also put Map, Object, Functions inside Array
+soupOfData.forEach(element => {
+    console.log(typeof element);
+});
