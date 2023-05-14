6de01fb7d71640e29c1c7832735e0c0ba3271467
Erkan <ErkanOzulu@users.noreply.github.com>
2023-05-14 12:10:03 +0300

    add:Arrays is start

---
+++ 05_Arrays.js
@@ -0,0 +1,19 @@
+/**
+ * There is no Collection in JS. In JS we only have Arrays and Map
+ * NOT Fixed size
+ * There are two important Brackets in JS 
+ * 1. [] ----> creates an Array
+ * 2. {} ----> creates an object
+ */
+
+let scores=new Array();
+let numbers= new Array(10);
+let emptyArray=[];
+let myNumbers=new Array(1,2,3,4,5);
+let colors=['red', 'blue','green'];
+
+console.log(scores);
+console.log(numbers);
+console.log(emptyArray);
+console.log(myNumbers);
+console.log(colors);
