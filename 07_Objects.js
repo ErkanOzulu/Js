58ba3849a8df7949d67da69ae7829464da0493f7
Erkan <ErkanOzulu@users.noreply.github.com>
2023-05-16 22:14:17 +0300

    object and live server

---
+++ 07_Objects.js
@@ -0,0 +1,26 @@
+let person={
+    firstName:"Joe",
+    lastName:'Doe'
+}
+console.log(typeof person);
+console.log(person);
+console.log(person.firstName);
+
+
+let adress={
+    'building no':5,
+     street:'Keowee',
+     state:'Kentucky'
+}
+
+console.log(adress);
+console.log(adress['building no']);
+console.log(adress.street);
+console.log(adress["street"]);
+
+// add new property to object
+
+person.age=30;
+console.log(person);
+
+//TODO https://javascript.info/ -->> useful for lerarning new things
