b697ee1f1669987428bbec2cac95ae87799f0433
Erkan <ErkanOzulu@users.noreply.github.com>
2023-05-16 21:25:40 +0300

    callBack hell

---
+++ callBack/CallBackSolution.js
@@ -0,0 +1,31 @@
+function waitingForSomeServerToAct(myCallBack) {
+    let dataNeeded;
+    setTimeout(() => {
+        console.log("BackEnd Information Have Just Been Received");
+        dataNeeded = "Cydeo";
+        myCallBack(dataNeeded)
+    }, 5000);
+    return dataNeeded;
+}
+
+function functionThatNeedsBackEndData(info) {
+    console.log("I need to use BackEnd Data "+info);
+}
+
+waitingForSomeServerToAct(functionThatNeedsBackEndData);
+
+
+//TODO I have some difficulties with callBack functions
+
+/**
+ * The first solution JS developers came up with was this callBack situation
+ * ES6 -- promises handles async nature of JS
+ * 2017 --- async functions, await keyword
+ * What is a callBack Hell?
+ * What is async code what is sync code?
+ */
+
+// alert("This is a JS alert");
+/**
+ * There are some JS commands like alert, prompt : these will work only in browsers with Live Server
+ */
